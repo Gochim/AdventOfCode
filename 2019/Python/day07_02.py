25d6e204ff0ce10405c4cc4729a36c9be6421d37
Gochim <ivad@protonmail.com>
2020-04-11 15:24:09 +0300

    Day 7 solution 2

--- 2019/Python/day07_02.py
+++ 2019/Python/day07_02.py
@@ -1,18 +1,12 @@
-import itertools
-
-
-def read_data_to_array(data_file):
-    data = open(data_file, 'r')
-    result = [int(x) for x in data.readline().split(",")]
-    data.close()
-
-    return result
-
+from itertools import permutations
+from file_utils import read_data_to_array
 
 PARAMETER_MODE_POSITION = 0
 PARAMETER_MODE_VALUE = 1
 
 COMMAND_HALT = 99
+COMMAND_INPUT = 3
+COMMAND_OUTPUT = 4
 
 
 def decode_command_params(code):
@@ -42,29 +36,28 @@ def get_data(input_data, index, param_mode):
 
 
 def command_add(input_data, index, com_params, adt_params):
-    result = get_data(input_data, index + 1, com_params[1]) + get_data(input_data, index + 2, com_params[2])
-    input_data[input_data[index + 3]] = result
+    first = get_data(input_data, index + 1, com_params[1])
+    second = get_data(input_data, index + 2, com_params[2])
+    input_data[input_data[index + 3]] = first + second
     return index + 4
 
 
 def command_mlt(input_data, index, com_params, adt_params):
     first = get_data(input_data, index + 1, com_params[1])
     second = get_data(input_data, index + 2, com_params[2])
-    result = first * second
-    input_data[input_data[index + 3]] = result
+    input_data[input_data[index + 3]] = first * second
     return index + 4
 
 
 def command_input(input_data, index, com_params, adt_params):
-    input_data[input_data[index + 1]] = input_params.pop(0)
+    input_data[input_data[index + 1]] = adt_params
     return index + 2
 
 
 def command_output(input_data, index, com_params, adt_params):
     diag_code = get_data(input_data, index + 1, com_params[1])
-    output_params.append(diag_code)
     print("{} after {} steps".format(diag_code, adt_params))
-    return index + 2
+    return index + 2, diag_code
 
 
 def command_jump_if_true(input_data, index, com_params, adt_params):
@@ -99,88 +92,104 @@ def command_equals(input_data, index, com_params, adt_params):
     return index + 4
 
 
-def command_(input_data, index, com_params):
+def command_halt(self, input_data, index, com_params):
+    return index
+
+
+def command_(self, input_data, index, com_params):
     pass
 
 
-input_params = []
-output_params = []
+class Amplifier:
+    def __init__(self, starting_data, phase_setting) -> None:
+        self._phase_setting = phase_setting
+        self._phase_setting_set = False
+        self._program = starting_data.copy()
+        self._output_params = [0]
+        self._input_value = self._index = self._steps = 0
 
-command_map = {
-    1: command_add,
-    2: command_mlt,
-    3: command_input,
-    4: command_output,
-    5: command_jump_if_true,
-    6: command_jump_if_false,
-    7: command_less_than,
-    8: command_equals
-}
+        self._command_map = {
+            1: command_add,
+            2: command_mlt,
+            3: command_input,
+            4: command_output,
+            5: command_jump_if_true,
+            6: command_jump_if_false,
+            7: command_less_than,
+            8: command_equals,
+            99: command_halt
+        }
 
+    def run_amplifier(self, input_value):
+        self._input_value = input_value
+        exit_op, output = self.execute_program()
+        return exit_op, output
 
-def execute_program(program):
-    index, steps = 0, 0
-    try:
-        while decode_command_params(program[index])[0] is not COMMAND_HALT:
-            com_params = decode_command_params(program[index])
+    def execute_program(self):
+        try:
+            while True:
+                com_params = decode_command_params(self._program[self._index])
 
-            func = command_map.get(com_params[0])
-            if func is None:
-                raise ValueError("Unknown command")
-            else:
-                index = func(program, index, com_params, (steps))
+                func = self._command_map.get(com_params[0])
+                if com_params[0] is COMMAND_INPUT:
+                    self._index = func(self._program, self._index, com_params, self._prepare_input_value())
+                elif com_params[0] is COMMAND_OUTPUT:
+                    (self._index, data) = func(self._program, self._index, com_params, self._steps)
+                    self._output_params = [data]
+                else:
+                    self._index = func(self._program, self._index, com_params, self._steps)
 
-            steps += 1
-    except:
-        print(" == Raised error on step {}".format(steps))
+                self._steps += 1
 
-    print("{} total steps".format(steps))
+                if com_params[0] in [COMMAND_HALT, COMMAND_OUTPUT]:
+                    break
+        except:
+            print(" == Raised error on step {}".format(self._steps))
 
+        # noinspection PyUnboundLocalVariable
+        return com_params[0], self._output_params[0]
 
-def run_amplifier(starting_data, phase_setting, input_value):
-    global input_params
-    global output_params
-    output_params.clear()
-    program = starting_data.copy()
-    input_params = [phase_setting, input_value]
-    execute_program(program)
-    return output_params[0]
+    def _prepare_input_value(self):
+        input_value = self._input_value if self._phase_setting_set else self._phase_setting
+        self._phase_setting_set = True
+        return input_value
 
 
 # Task - https://adventofcode.com/2019/day/7
 def main():
     starting_data = read_data_to_array("day07.txt")
     expected_output = None
-    # starting_data = [3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0]
-    # expected_output = 43210
-    # starting_data = [3, 23, 3, 24, 1002, 24, 10, 24, 1002, 23, -1, 23, 101, 5, 23, 23, 1, 24, 23, 23, 4, 23, 99, 0, 0]
-    # expected_output = 54321
-    # starting_data = [3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33,
-    #                  1002, 33, 7, 33, 1, 33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0]
-    # expected_output = 65210
 
-    phase_settings = [0, 1, 2, 3, 4]
+    # Tests
+    # starting_data = [3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1, 27, 26,
+    #                  27, 4, 27, 1001, 28, -1, 28, 1005, 28, 6, 99, 0, 0, 5]
+    # expected_output = 139629729
+
+    # starting_data = [3, 52, 1001, 52, -5, 52, 3, 53, 1, 52, 56, 54, 1007, 54, 5, 55, 1005, 55, 26, 1001, 54,
+    #                  -5, 54, 1105, 1, 12, 1, 53, 54, 53, 1008, 54, 0, 55, 1001, 55, 1, 55, 2, 53, 55, 53, 4,
+    #                  53, 1001, 56, -1, 56, 1005, 56, 6, 99, 0, 0, 0, 0, 10]
+    # expected_output = 18216
+
+    phase_settings = [5, 6, 7, 8, 9]
+    amp_count = len(phase_settings)
+
     max_value = 0
-    max_settings = []
-    for subset in itertools.permutations(phase_settings, len(phase_settings)):
-        cur_value = run_amplifier(starting_data, subset[0], 0)
-        cur_value = run_amplifier(starting_data, subset[1], cur_value)
-        cur_value = run_amplifier(starting_data, subset[2], cur_value)
-        cur_value = run_amplifier(starting_data, subset[3], cur_value)
-        cur_value = run_amplifier(starting_data, subset[4], cur_value)
+    for phase_settings in permutations(phase_settings, amp_count):
+        cur_value = return_code = 0
+        amplifiers = [Amplifier(starting_data, phase_settings[i]) for i in range(amp_count)]
 
-        if cur_value > max_value:
-            max_value = cur_value
-            max_settings = subset
+        while return_code != COMMAND_HALT:
+            for i in range(amp_count):
+                return_code, cur_value = amplifiers[i].run_amplifier(cur_value)
+
+        max_value = max(cur_value, max_value)
+
+    print(max_value)
 
     if expected_output is not None:
         print("=== Output is correct {}".format(max_value)) if expected_output == max_value else \
-            print("Received {} instead of {}".format(expected_output, max_value))
-    print(max_value)
-    print(max_settings)
+            print("Received {} instead of {}".format(max_value, expected_output))
 
 
 if __name__ == '__main__':
     main()
-
-
