b5f993b8f5e772372c12479f839fbe2ab3ea8857
Gochim <ivad@protonmail.com>
2020-04-22 18:35:07 +0300

    Day 1 part 1 simplified

--- 2019/Python/day01_01.py
+++ 2019/Python/day01_01.py
@@ -4,13 +4,10 @@ import math
 # Task - https://adventofcode.com/2019/day/1
 def main():
     data = open("day01_01.txt", 'r')
-    final = 0
-    for mass in data:
-        fuel = math.trunc(int(mass) / 3) - 2
-        final = final + fuel
+    fuels = [(math.trunc(int(mass) / 3) - 2) for mass in data]
+    final = sum(fuels)
     data.close()
     print(final)
-    return
 
 
 if __name__ == '__main__':
